519eec1e3f82bea522f18ca7142af86b0d45df0d
Henry Senior <hps1945@live.co.uk>
2021-03-14 22:25:40 +0000

    argument parser complete

---
+++ src/console/console.py
@@ -0,0 +1,10 @@
+"""A class to represent the console application"""
+
+class Console:
+    """A class to represent the console application"""
+    def __init__(self, file_path):
+        super().__init__()
+        print(file_path)
+
+    def do_something(self):
+        print("I did it!")
