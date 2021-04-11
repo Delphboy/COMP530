ab22dd49cbc9f37095b6d00b5e28a640c79fa702
Henry Senior <hps1945@live.co.uk>
2021-04-11 11:12:51 +0100

    tidied up UI implementation

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,17 +1,17 @@
+"""Module to hold the GUI functionality"""
 import sys
-
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+#from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5 import QtWidgets, uic
 
 class InterfaceWrapper(QtWidgets.QMainWindow):
     """Provides a wrapper for the UI"""
     def __init__(self):
         super(InterfaceWrapper, self).__init__()
-        uic.loadUi('C:\\Users\\HPS19\\Desktop\\comp530-ui.ui', self)
+        uic.loadUi('src/gui/ui.ui', self)
         self.show()
 
 
 def show_gui():
     app = QtWidgets.QApplication(sys.argv)
-    window = InterfaceWrapper()
+    _ = InterfaceWrapper()
     app.exec_()
--- src/main.py
+++ src/main.py
@@ -1,15 +1,8 @@
 """Entry point for the console application"""
 import argparse
-import os
-import sys
 
 from gui import gui
-
-
 from console import console
-
-
-
 
 def main():
     parser = argparse.ArgumentParser(
@@ -42,26 +35,7 @@ def main():
     elif args.test is not None:
         print("Testing model based on dataset: " + args.test)
     elif args.gui is True:
-        # print("Start the GUI once it has been developed")
-        # app = QApplication(sys.argv)
-        # window = QWidget()
-        # message_label = QLabel(window)
-        # message_label.setText("Hello World!")
-        # window.setGeometry(100,100,200,50)
-        # message_label.move(50,20)
-        # window.setWindowTitle("PyQt5")
-        # window.show()
-        # sys.exit(app.exec_())
-        
         gui.show_gui()
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
